d75c02be6254539a7a56e3d80b3afcc56011064d
Dwarf3d <Dwarf3d@gmail.com>
2016-09-15 16:53:04 -0400

    Added a Feedback.txt from sprint0 and fixed the escape key quitting the game.

--- MarioGame/Core/Game1.cs
+++ MarioGame/Core/Game1.cs
@@ -73,10 +73,6 @@ namespace MarioGame.Core
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) ||
-                Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             _scenes[_scene - 1].Update(gameTime);
 
             base.Update(gameTime);
