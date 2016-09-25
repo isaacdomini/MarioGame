1ba5dc5afae3970f3aebc164154215cdf3b3b6ba
JohnSimerlink <johnsimerlink@gmail.com>
2016-09-25 17:32:16 -0400

    direction and frames for mario

--- MarioGame/Entities/Entity.cs
+++ MarioGame/Entities/Entity.cs
@@ -21,25 +21,6 @@ namespace MarioGame.Entities
             Left = 1,
             Right = 2
         }
-        protected enum Frames
-        {
-            //frames are all facing left. Except DeadMario who is facing the computer user.
-            DeadMario = 1,
-            FlyingMarioEnd = 2,
-            FlyingMarioBeforeEnd = 3,
-            FlyingMarioMiddle = 4,
-            FlyingMarioAfterStart = 5,
-            FlyingMarioStart = 6,
-            SittingMario2 = 7,
-            SittingMario1 = 8,
-            JumpingMario = 9,
-            UnkownAction3 = 10,
-            WalkingMario3 = 11,
-            WalkingMario2 = 12,
-            WalkingMario1 = 13,
-            StandingMario = 14
-
-        }
         public virtual void ChangeState(MarioState newstate)
         {
             throw new NotImplementedException();
--- MarioGame/Entities/PlayerEntities/MarioEntity.cs
+++ MarioGame/Entities/PlayerEntities/MarioEntity.cs
@@ -8,6 +8,24 @@ namespace MarioGame.Entities.PlayerEntities
 {
     public class MarioEntity : Entity
     {
+        protected enum Frames
+        {
+            //frames are all facing left. Except DeadMario who is facing the computer user.
+            DeadMario = 1,
+            FlyingMarioEnd = 2,
+            FlyingMarioBeforeEnd = 3,
+            FlyingMarioMiddle = 4,
+            FlyingMarioAfterStart = 5,
+            FlyingMarioStart = 6,
+            SittingMario2 = 7,
+            SittingMario1 = 8,
+            JumpingMario = 9,
+            UnkownAction3 = 10,
+            WalkingMario3 = 11,
+            WalkingMario2 = 12,
+            WalkingMario1 = 13,
+            StandingMario = 14
+        }
         public MarioEntity()
         {
             _state = new IdleMarioState(this);
