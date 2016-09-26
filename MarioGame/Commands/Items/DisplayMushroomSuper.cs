ac36b21048e98ed011332f47900426f0b821149d
Erin George <george.730@osu.edu>
2016-09-25 20:38:31 -0400

    Items added

---
+++ MarioGame/Commands/Items/DisplayMushroomSuper.cs
@@ -0,0 +1,24 @@
+﻿using MarioGame.Theming.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioGame.Commands
+{
+    class DisplayMushroomSuper : ICommand
+    {
+        public DisplayMushroomSuper(Scene scene)
+        {
+            Scene = scene;
+        }
+
+        public Scene Scene { get; }
+
+        public void Execute()
+        {
+            Scene.ShowItems(Scene.ItemTypes.MushroomSuper.GetHashCode());
+        }
+    }
+}
---
+++ MarioGame/Commands/Items/DisplayStar.cs
@@ -0,0 +1,24 @@
+﻿using MarioGame.Theming.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioGame.Commands
+{
+    class DisplayStar : ICommand
+    {
+        public DisplayStar(Scene scene)
+        {
+            Scene = scene;
+        }
+
+        public Scene Scene { get; }
+
+        public void Execute()
+        {
+            Scene.ShowItems(Scene.ItemTypes.Star.GetHashCode());
+        }
+    }
+}
