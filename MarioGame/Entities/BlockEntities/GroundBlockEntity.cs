0506de8a605022f8e12254cbe7fbfbdd7a45e84c
JohnSimerlink <johnsimerlink@gmail.com>
2016-09-28 16:32:35 -0400

    file rename

--- MarioGame/Entities/BlockEntities/GroundBlockEntity.cs
+++ MarioGame/Entities/BlockEntities/GroundBlockEntity.cs
@@ -3,16 +3,15 @@ using MarioGame.Sprites;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using MarioGame.Sprites.BlockSprites;
+using MarioGame.Entities.BlockEntities;
 
-namespace MarioGame.Entities.ItemEntities
+namespace MarioGame.Entities.BLockEntitiesEntities
 {
-    public class GroundBlockEntity : Entity
+    public class GroundBlockEntity : BlockEntity
     {
-        // Could be useful for casting in certain circumstances
-        ContentManager content;
         public GroundBlockEntity(Vector2 position, GroundBlockSprite sprite) : base(position, sprite)
         {
         }
-        public override void Update() { }
     }
 }
