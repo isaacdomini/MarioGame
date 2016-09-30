6e69344a1ef9b0fe575f4c8d2f76f4563b85cd1c
JohnSimerlink <johnsimerlink@gmail.com>
2016-09-30 17:35:33 -0400

    dont reset animation if hitting left when already moving left

--- MarioGame/States/PlayerStates/ActionStates/WalkingMarioState.cs
+++ MarioGame/States/PlayerStates/ActionStates/WalkingMarioState.cs
@@ -44,15 +44,7 @@ namespace MarioGame.States.PlayerStates
         }
         public override void MoveLeft()
         {
-            if (this.direction == Directions.Left)
-            {
-                MarioActionState moveLeft = new WalkingMarioState(marioEntity);
-                marioEntity.ChangeActionState(moveLeft);
-                moveLeft.setDirection(Directions.Left);
-                marioEntity.setVelocity(MarioEntity.walkingLeftVelocity);
-                moveLeft.Begin(this);
-            }
-            else if (this.direction == Directions.Right)
+            if (this.direction == Directions.Right)
             {
                 MarioActionState idleRight = new IdleMarioState(marioEntity);
                 marioEntity.ChangeActionState(idleRight);
@@ -64,17 +56,6 @@ namespace MarioGame.States.PlayerStates
         }
         public override void MoveRight()
         {
-            // Meaning idle mario is already facing right
-            if (this.direction == Directions.Right)
-            {
-                // Mario state is set to walking right
-                MarioActionState moveRight = new WalkingMarioState(marioEntity);
-                marioEntity.ChangeActionState(moveRight);
-                moveRight.setDirection(Directions.Right);
-                marioEntity.setVelocity(MarioEntity.walkingRightVelocity);
-                moveRight.Begin(this);
-            }
-            // Meaning mario is facing left walking
             else if (this.direction == Directions.Left)
             {
                 // Mario now is idles left
