ffe82252f71c6fc0ad88055e14d07a84afed136c
JohnSimerlink <johnsimerlink@gmail.com>
2016-10-18 15:41:28 -0400

    create boilerplate for block power up state - hiiden and shown

---
+++ MarioGame/States/PlayerStates/BlockPowerUpState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioGame.Entities;
+
+namespace MarioGame.States.PlayerStates
+{
+    public class BlockPowerUpState : PowerUpState
+    {
+        public BlockPowerUpState(IEntity entity) : base(entity)
+        {
+        }
+    }
+}
