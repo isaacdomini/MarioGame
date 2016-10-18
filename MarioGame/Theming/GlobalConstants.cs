ed9fda03a8887a0b5192a0950db843bd25e34ef3
Ricky Renner <rennerricky1@gmail.com>
2016-10-18 16:59:31 -0400

    Added global constants file.

---
+++ MarioGame/Theming/GlobalConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioGame.Theming
+{
+    public static class GlobalConstants
+    {
+        public static int GridHeight = 30;
+        public static int GridWidth = 30;
+    }
+}
