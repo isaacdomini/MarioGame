1bfce644ce5c11cdda35cfee3747495b6345d467
Cyriac Domini <thundathil.4@buckeyemail.osu.edu>
2016-11-01 08:22:34 -0400

    json file

---
+++ MarioGame/Sprites/BlockSprites/PipeSectionSprite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioGame.Entities;
+using Microsoft.Xna.Framework.Content;
+using MarioGame.States;
+
+namespace MarioGame.Sprites
+{
+    public class PipeSectionSprite : BlockSprite
+    {
+        public enum Frames
+        {
+            StepBlock = 0
+        }
+        public PipeSectionSprite(ContentManager content, Entity entity) : base(content, entity)
+        {
+            AssetName = "pipesection";
+            NumberOfFramesPerRow = 1;
+            //Each state has a frameSet
+            FrameSets = new Dictionary<int, List<int>> {
+                { BlockActionStateEnum.Standard.GetHashCode(), new List<int> { Frames.StepBlock.GetHashCode() } }
+            };
+            FrameSet = FrameSets[BlockActionStateEnum.Standard.GetHashCode()];
+        }
+    }
+}
