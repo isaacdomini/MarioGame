e9844e3fd4422f6f19d55c83704bead23799a43f
JohnSimerlink <johnsimerlink@gmail.com>
2016-11-13 21:59:14 -0500

    bash script to autocopy json files. to use run  if you have bash installed

---
+++ copyJsonFiles.sh
@@ -0,0 +1,6 @@
+cp MarioGame/Theming/Scenes/Level1.json MarioGame/bin/Windows/x86/Debug/Level1.json
+cp MarioGame/Theming/Scenes/HiddenLevel.json MarioGame/bin/Windows/x86/Debug/HiddenLevel.json
+cp MarioGame/Theming/Scenes/Level2.json MarioGame/bin/Windows/x86/Debug/Level2.json
+
+
+
