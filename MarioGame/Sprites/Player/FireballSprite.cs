c66e8a6aa157c3d9ba82f61598920c1adc980fa9
JohnSimerlink <johnsimerlink@gmail.com>
2016-11-15 11:12:14 -0500

    fireball sprite and entity boilerplate

---
+++ MarioGame/Sprites/Player/FireballSprite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioGame.Entities;
+using Microsoft.Xna.Framework.Content;
+
+namespace MarioGame.Sprites.Player
+{
+    public class FireballSprite: AnimatedSprite
+    {
+        public FireballSprite(ContentManager content, Entity entity) : base(content, entity)
+        {
+        }
+    }
+}
