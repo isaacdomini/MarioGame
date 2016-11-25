a27eab753c9ab811c5ebaf8b61ac0bc66a3a642f
JohnSimerlink <johnsimerlink@gmail.com>
2016-11-25 09:54:16 -0500

    remade keypress proof of concept

---
+++ ExternalAI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Threading;
+
+namespace ExternalAI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            InputSimulator input = new InputSimulator();
+            Thread.Sleep(3000);
+            input.Keyboard.KeyDown(VirtualKeyCode.VK_D);
+            Thread.Sleep(2000);
+            input.Keyboard.KeyUp(VirtualKeyCode.VK_D);
+        }
+    }
+}
