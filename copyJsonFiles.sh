df64c90002fd9ced56a73d43cc21d49a623eebfc
JohnSimerlink <johnsimerlink@gmail.com>
2016-11-25 09:30:56 -0500

    added mainmenu in copy json script

--- copyJsonFiles.sh
+++ copyJsonFiles.sh
@@ -2,5 +2,6 @@ cp MarioGame/Theming/Scenes/Level1.json MarioGame/bin/Windows/x86/Debug/Level1.j
 cp MarioGame/Theming/Scenes/HiddenLevel.json MarioGame/bin/Windows/x86/Debug/HiddenLevel.json
 cp MarioGame/Theming/Scenes/Level2.json MarioGame/bin/Windows/x86/Debug/Level2.json
 cp MarioGame/Theming/Scenes/GameOver.json MarioGame/bin/Windows/x86/Debug/GameOver.json
+cp MarioGame/Theming/Scenes/MainMenu.json MarioGame/bin/Windows/x86/Debug/MainMenu.json
 
 
